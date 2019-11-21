bf3b8f003213ad7fe8aee7151a6cf42966052c10
MelodRAMAtick <melodramatick@users.noreply.github.com>
2019-11-21 20:43:53 -0300

    Botones despliegan containers adecuados
    Faltaría la funcionalidad de los Dropdowns (que aparezcan las opciones que tiene cada botón), pero al menos al presionar en ellos se renderiza el contenido que pedí.

--- src - FrontEnd/Principal.js
+++ src - FrontEnd/Principal.js
@@ -1,16 +1,108 @@
 import './bootstrap.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
-// class PaginaLogIn extends React.Component{
-//     render(){
-//         return(
-//             <div>
-//                 <input placeholder= "Ingrese usuario">usuario</input>
-//                 <input placeholder=" Ingrese contraseña">contraseña</input>
-//             </div>
-//         );
-//     }
-// }
+
+function MenúAgregar(){
+    ReactDOM.render(
+            <div>
+                <h2>Menú "Agregar"</h2>
+                <form id="Agregar">
+                    <ul>
+                        <li className="form-group">
+                        <label for="addNombre"> <strong>Nombre:</strong></label>
+                            <br></br>
+                            <input type="text" id="addNombre" name="nombre"></input>
+                        </li>
+                        <li className="form-group">
+                            <label for="addApellido"> <strong>Apellido:</strong></label>
+                                <br></br>
+                            <input type="text" id="addApellido" name="apellido"></input>
+                        </li>
+
+                        <li className="form-group">
+                            <label for="inputState"><strong>Elija un <em>tipo de usuario</em></strong></label>
+                            <select id="inputState" class="form-control" style={{width: '182.5px'}}>
+                                <option selected>Elija una opción...</option>
+                                <option>Persona</option>
+                                <option>Inquilino</option>
+                                <option>Dueño</option>
+                            </select>
+                        </li>
+
+                        <li className="form-group">
+                            <label for="addEdificio"> <strong>Edificio:</strong></label>
+                                <br></br>
+                            <input type="text" id="addEdificio" name="edificio"></input>
+                        </li>
+
+                        <li className="form-group">
+                            <label for="Reclamo"> <strong>Descripción del reclamo:</strong></label>
+                                <br></br>
+                            <textarea type="text-area" id="addReclamo" name="reclamo" style={{width:'25%'}}></textarea>
+                        </li>
+                    </ul>
+                </form>
+            </div>, document.getElementById("containerAdd"));
+}
+
+function MenúBuscar(){
+    ReactDOM.render(
+        <div>
+            <h2>Menú "Buscar"</h2>
+            <form>
+                <ul>
+                    <li className="form-group">
+                    <label for="addNombre"> <strong>Nombre:</strong></label>
+                        <br></br>
+                        <input type="text" id="addNombre" name="nombre"></input>
+                    </li>
+                    <li className="form-group">
+                        <label for="addApellido"> <strong>Apellido:</strong></label>
+                            <br></br>
+                        <input type="text" id="addApellido" name="apellido"></input>
+                    </li>
+
+                    <li className="form-group">
+                        <label for="inputState"><strong>Elija un <em>tipo de usuario</em></strong></label>
+                        <select id="inputState" class="form-control" style={{width: '182.5px'}}>
+                            <option selected>Elija una opción...</option>
+                            <option>Persona</option>
+                            <option>Inquilino</option>
+                            <option>Dueño</option>
+                        </select>
+                    </li>
+
+                    <li className="form-group">
+                        <label for="addEdificio"> <strong>Edificio:</strong></label>
+                            <br></br>
+                        <input type="text" id="addEdificio" name="edificio"></input>
+                    </li>
+
+                    <li className="form-group">
+                        <div class="form-row">
+                            <div class="col col-md-2">
+                                <label for="inputState">Piso:</label>
+                                <select id="inputState" class="form-control">
+                                    <option selected>Elija un piso...</option> 
+                                    {/*Varían la cantidad de opciones en base a la cantidad de pisos que tenga el edificio*/}
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="col col-md-2">
+                                <label for="inputState">Unidad:</label>
+                                <select id="inputState" class="form-control">
+                                    <option selected>Elija un N° de unidad...</option> 
+                                        {/*Varían la cantidad de opciones en base a la cantidad de unidades que haya en el piso*/}
+                                    <option>...</option>
+                                </select>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </form>
+        </div>, document.getElementById("containerGet"));
+}
+
 class MainPage extends React.Component{
     render(){
         return(     
@@ -19,7 +111,7 @@ class MainPage extends React.Component{
                     <h1 className="text-center">Sistema Administrador de Edificios, Personas y Reclamos</h1>
                     <div className="row">
                         <div className="col col-md-1 md-offset-4 dropdown">
-                            <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                            <button type="button" className="btn btn-primary dropdown-toggle" onClick={MenúAgregar}>
                             Agregar
                             </button>
                             <div class="dropdown-menu">
@@ -29,7 +121,7 @@ class MainPage extends React.Component{
                             </div>
                         </div>
                         <div className="col col-md-1 dropdown">
-                            <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                            <button type="button" className="btn btn-primary dropdown-toggle" onClick={MenúBuscar}>
                             Buscar
                             </button>
                             <div class="dropdown-menu">
@@ -43,98 +135,11 @@ class MainPage extends React.Component{
                 </div>
 
                 <div>
-                    <h2>Menú "Agregar"</h2>
-                    <form id="Agregar">
-                        <ul>
-                            <li className="form-group">
-                            <label for="addNombre"> <strong>Nombre:</strong></label>
-                              <br></br>
-                              <input type="text" id="addNombre" name="nombre"></input>
-                            </li>
-                            <li className="form-group">
-                                <label for="addApellido"> <strong>Apellido:</strong></label>
-                                    <br></br>
-                                <input type="text" id="addApellido" name="apellido"></input>
-                            </li>
-
-                            <li className="form-group">
-                                <label for="inputState"><strong>Elija un <em>tipo de usuario</em></strong></label>
-                                <select id="inputState" class="form-control" style={{width: '182.5px'}}>
-                                    <option selected>Elija una opción...</option>
-                                    <option>Persona</option>
-                                    <option>Inquilino</option>
-                                    <option>Dueño</option>
-                                </select>
-                            </li>
-
-                            <li className="form-group">
-                                <label for="addEdificio"> <strong>Edificio:</strong></label>
-                                    <br></br>
-                                <input type="text" id="addEdificio" name="edificio"></input>
-                            </li>
-
-                            <li className="form-group">
-                                <label for="Reclamo"> <strong>Descripción del reclamo:</strong></label>
-                                    <br></br>
-                                <textarea type="text-area" id="addReclamo" name="reclamo" style={{width:'25%'}}></textarea>
-                            </li>
-                        </ul>
-                    </form>
+                    <form id="containerAdd"></form>
                 </div>
                 
                 <div>
-                    <h2>Menú "Buscar"</h2>
-                    <form id="Agregar">
-                        <ul>
-                            <li className="form-group">
-                            <label for="addNombre"> <strong>Nombre:</strong></label>
-                              <br></br>
-                              <input type="text" id="addNombre" name="nombre"></input>
-                            </li>
-                            <li className="form-group">
-                                <label for="addApellido"> <strong>Apellido:</strong></label>
-                                    <br></br>
-                                <input type="text" id="addApellido" name="apellido"></input>
-                            </li>
-
-                            <li className="form-group">
-                                <label for="inputState"><strong>Elija un <em>tipo de usuario</em></strong></label>
-                                <select id="inputState" class="form-control" style={{width: '182.5px'}}>
-                                    <option selected>Elija una opción...</option>
-                                    <option>Persona</option>
-                                    <option>Inquilino</option>
-                                    <option>Dueño</option>
-                                </select>
-                            </li>
-
-                            <li className="form-group">
-                                <label for="addEdificio"> <strong>Edificio:</strong></label>
-                                    <br></br>
-                                <input type="text" id="addEdificio" name="edificio"></input>
-                            </li>
-
-                            <li className="form-group">
-                                <div class="form-row">
-                                    <div class="col col-md-2">
-                                        <label for="inputState">Piso:</label>
-                                        <select id="inputState" class="form-control">
-                                            <option selected>Elija un piso...</option> 
-                                            {/*Varían la cantidad de opciones en base a la cantidad de pisos que tenga el edificio*/}
-                                            <option>...</option>
-                                        </select>
-                                    </div>
-                                    <div class="col col-md-2">
-                                        <label for="inputState">Unidad:</label>
-                                        <select id="inputState" class="form-control">
-                                            <option selected>Elija un N° de unidad...</option> 
-                                             {/*Varían la cantidad de opciones en base a la cantidad de unidades que haya en el piso*/}
-                                            <option>...</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </form>
+                    <form id="containerGet"></form>
                 </div>
             </body>
         );
